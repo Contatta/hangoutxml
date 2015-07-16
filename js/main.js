32f0060f8a1d544df54896c5739b33996f4c150c
jrschifa <jeffschifano@gmail.com>
2015-07-16 11:42:07 -0700

    add existing participants to the list when joining the hangout

--- js/main.js
+++ js/main.js
@@ -2,75 +2,23 @@
     var endpoint = 'https://{instance}.ryver.com/api/1/odata.svc/workrooms({id})/Chat.PostMessage',
         state = null;
 
-    function setTitle() {
-        if (state && state.descriptor)
-            $('#ryver-room').text(state.descriptor);
-    }
+    function consumer(data) {
+        console.debug('data: ', data);
+        console.debug('endpoint: ', endpoint);
+        console.debug('state: ', state);
 
-    function onStateChange(evt) {
-        console.debug('state changed: ', evt);
-        state = evt.state;
-    }
-
-    function onParticipantsChange(evt) {
-        console.debug('participants changed: ', evt);
-    }
-
-    function onParticipantsAdded(evt) {
-        // todo: set room descriptor in header bar
-        console.debug('participants added: ', evt);
-        var participants = evt.addedParticipants;
-
-        for (var i=0; i < participants.length; i++) {
-            var person = participants[i]['person'],
-                avatar = $('<img />').addClass('participant-list__item-avatar').attr('src', person['image']['url']),
-                content = $('<span />').addClass('participant-list__item-content').text(person.displayName),
-                item = $('<li />').addClass('participant-list__item').attr('data-id', person.id);
-
-            item.append(avatar, content);
-
-            $('#ryver-participant-list').append(item);
+        if (data && endpoint) {
+            return $.ajax({
+                url: endpoint,
+                type: 'POST',
+                contentType: 'application/json',
+                accepts: {'json': 'application/json'},
+                data: JSON.stringify(data),
+                headers: {
+                    'Contatta-Session': state.session
+                }
+            });
         }
-    }
-
-    function onParticipantsRemoved(evt) {
-        console.debug('participants removed: ', evt);
-
-        var participants = evt.removedParticipants;
-
-        for (var i=0; i < participants.length; i++) {
-            var person = participants[i]['person'];
-
-            $("[data-id]='" + person.id + "'").fadeOut().remove();
-        }
-    }
-
-    function onApiReady(evt) {
-        if (evt.isApiReady) {
-            console.debug('api ready: ', evt);
-            var startData = google.hangout.getStartData();
-
-            if (startData) {
-                state = JSON.parse(startData);
-                updateSharedState(state);
-                setupEndpoint();
-                consumer({'body': google.hangout.getHangoutUrl()});
-            } else {
-                state = google.hangout.data.getState();
-            }
-
-            setTitle();
-
-            google.hangout.data.onStateChanged.add(onStateChange);
-            google.hangout.onParticipantsChanged.add(onParticipantsChange);
-            google.hangout.onParticipantsAdded.add(onParticipantsAdded);
-            google.hangout.onParticipantsRemoved.add(onParticipantsRemoved);
-            google.hangout.onApiReady.remove(onApiReady);
-        }
-    }
-
-    function updateSharedState(data) {
-        google.hangout.data.submitDelta(data);
     }
 
     function setupEndpoint() {
@@ -88,22 +36,79 @@
         });
     }
 
-    function consumer(data) {
-        console.debug('data: ', data);
-        console.debug('endpoint: ', endpoint);
-        console.debug('state: ', state);
+    function setTitle() {
+        if (state && state.descriptor)
+            $('#ryver-room').text(state.descriptor);
+    }
 
-        if (data && endpoint) {
-            return $.ajax({
-                url: endpoint,
-                type: 'POST',
-                contentType: 'application/json',
-                accepts: {'json': 'application/json'},
-                data: JSON.stringify(data),
-                headers: {
-                    'Contatta-Session': state.session
-                }
-            });
+    function addParticipants(participants) {
+        participants = participants || [];
+
+        for (var i=0; i < participants.length; i++) {
+            var person = participants[i]['person'],
+                avatar = $('<img />').addClass('participant-list__item-avatar').attr('src', person['image']['url']),
+                content = $('<span />').addClass('participant-list__item-content').text(person.displayName),
+                item = $('<li />').addClass('participant-list__item').attr('data-id', person.id);
+
+            item.append(avatar, content);
+
+            $('#ryver-participant-list').append(item);
+        }
+    }
+
+    function removeParticipants(participants) {
+        for (var i=0; i < participants.length; i++) {
+            var person = participants[i]['person'];
+
+            $("[data-id]='" + person.id + "'").fadeOut().remove();
+        }
+    }
+
+    function updateSharedState(data) {
+        google.hangout.data.submitDelta(data);
+    }
+
+    function onStateChange(evt) {
+        console.debug('state changed: ', evt);
+        state = evt.state;
+    }
+
+    function onParticipantsChange(evt) {
+        console.debug('participants changed: ', evt);
+    }
+
+    function onParticipantsAdded(evt) {
+        console.debug('participants added: ', evt);
+        addParticipants(evt.addedParticipants);
+    }
+
+    function onParticipantsRemoved(evt) {
+        console.debug('participants removed: ', evt);
+        removeParticipants(evt.removedParticipants);
+    }
+
+    function onApiReady(evt) {
+        if (evt.isApiReady) {
+            console.debug('api ready: ', evt);
+            var startData = google.hangout.getStartData();
+
+            if (startData) {
+                state = JSON.parse(startData);
+                updateSharedState(state);
+                setupEndpoint();
+                consumer({'body': google.hangout.getHangoutUrl()});
+            } else {
+                state = google.hangout.data.getState();
+                addParticipants(google.hangout.getParticipants());
+            }
+
+            setTitle();
+
+            google.hangout.data.onStateChanged.add(onStateChange);
+            google.hangout.onParticipantsChanged.add(onParticipantsChange);
+            google.hangout.onParticipantsAdded.add(onParticipantsAdded);
+            google.hangout.onParticipantsRemoved.add(onParticipantsRemoved);
+            google.hangout.onApiReady.remove(onApiReady);
         }
     }
 
